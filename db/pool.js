5dbb800bef667a100b4bbe1e6b3634531cd2061c
Preslav977 <p.cvetanow@gmail.com>
2025-07-29 10:24:44 +0300

    Installed the necessary packages, and created db folder with few files to export the prisma client and import it in other files later

---
+++ db/pool.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+
+const { Pool } = require("pg");
+
+module.exports = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    require: true,
+  },
+});
---
+++ db/populatedb.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const { Client } = require("pg");
+
+const SQL = ``;
+
+async function main() {
+  try {
+    console.log("seeding...");
+
+    const client = new Client({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        require: true,
+      },
+    });
+    await client.connect();
+
+    await client.query(SQL);
+
+    await client.end();
+
+    console.log("done");
+  } catch (err) {
+    console.error("An error occurred when seeing", err);
+    throw err;
+  }
+}
+
+main();
