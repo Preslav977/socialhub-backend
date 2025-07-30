e58f2d0a1a466000e14be4a5867ef01e50a30add
Preslav977 <p.cvetanow@gmail.com>
2025-07-30 09:16:33 +0300

    Created userSignUp middleware to validate the some of the fields, and validateMiddleware folder

---
+++ validatingMiddlewares/userSignUp.js
@@ -0,0 +1,65 @@
+const { body } = require("express-validator");
+
+const lengthError = "must be between 1 and 30 characters";
+
+const takenError = "is already taken";
+
+const passwordError =
+  "must be minimum 8 characters, and contain at least one letter, and one number";
+
+const passwordMatchError = "must match";
+
+const prisma = require("../db/client");
+
+const userSignUp = [
+  body("username")
+    .trim()
+    .isLength({ min: 1, max: 30 })
+    .escape()
+    .withMessage(`Username ${lengthError}`),
+
+  body("username").custom(async (value) => {
+    const isUsernameTaken = await prisma.user.findUnique({
+      where: {
+        username: value,
+      },
+    });
+
+    if (isUsernameTaken) {
+      throw new Error(`First name ${takenError}`);
+    }
+  }),
+
+  body("display_name")
+    .trim()
+    .isLength({ min: 1, max: 30 })
+    .escape()
+    .withMessage(`Display name ${lengthError}`),
+
+  body("display_name").custom(async (value) => {
+    const isDisplayNameTaken = await prisma.user.findUnique({
+      where: {
+        display_name: value,
+      },
+    });
+
+    if (isDisplayNameTaken) {
+      throw new Error(`Display name ${takenError}`);
+    }
+  }),
+
+  body("password")
+    .trim()
+    .isLength({ min: 8 })
+    .escape()
+    .matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)
+    .withMessage(`Password ${passwordError}`),
+
+  body("confirm_password")
+    .custom((value, { req }) => {
+      return value === req.body.password;
+    })
+    .withMessage(`Password ${passwordMatchError}`),
+];
+
+module.exports = userSignUp;
