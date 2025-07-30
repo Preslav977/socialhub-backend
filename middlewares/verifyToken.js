1824d130320cbd5567e7b00f65fcff96e8b81185
Preslav977 <p.cvetanow@gmail.com>
2025-07-30 11:16:05 +0300

    Created verifyToken middleware to check if the user is authenticated for certain routes

---
+++ middlewares/verifyToken.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers.authorization;
+
+  if (typeof bearerHeader !== "undefined") {
+    const bearer = bearerHeader.split(" ");
+
+    const bearerToken = bearer[1];
+
+    req.token = bearerToken;
+
+    jwt.verify(req.token, process.env.SECRET, (err, authData) => {
+      if (err) {
+        res.sendStatus(403);
+      }
+
+      req.authData = authData;
+
+      next();
+    });
+  }
+
+  res.sendStatus(403);
+}
+
+module.exports = verifyToken;
