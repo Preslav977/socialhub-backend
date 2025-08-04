4ee522f26612a83ee3e53d2ea03c32a606074644
Preslav977 <p.cvetanow@gmail.com>
2025-08-04 10:54:48 +0300

    Changed the max length related to the post content to be 2000 max

--- validatingMiddlewares/creatingPost.js
+++ validatingMiddlewares/creatingPost.js
@@ -1,11 +1,11 @@
 const { body } = require("express-validator");
 
-const lengthError = "must be between 1 and 30 characters";
+const lengthError = "must be between 1 and 2000 characters";
 
 const creatingPost = [
   body("post_content")
     .trim()
-    .isLength({ min: 1, max: 30 })
+    .isLength({ min: 1, max: 2000 })
     .escape()
     .withMessage(`Post content $${lengthError}`),
 
